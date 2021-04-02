bccdf81b10c43a67e22a6a93473cb7f777a646bd
Matt Brown <matthewbrown84@gmail.com>
2021-04-02 09:12:33 -0400

    Testing first pass

--- EmployeeSQL/1_Data_Engineering.sql
+++ EmployeeSQL/1_Data_Engineering.sql
@@ -1,6 +1,8 @@
 ﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 CREATE TABLE "departments" (
     "dept_no" VARCHAR(4)   NOT NULL,
@@ -15,17 +17,28 @@ CREATE TABLE "departments" (
 
 CREATE TABLE "dept_emp" (
     "emp_no" int   NOT NULL,
-    "dept_no" VARCHAR(4)   NOT NULL
+    "dept_no" VARCHAR(4)   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
 );
 
+-- One EE/Dept assign per EE.
+-- Any Dept may have more than one EE.
 CREATE TABLE "dept_manager" (
     "dept_no" VARCHAR(4)   NOT NULL,
-    "emp_no" int   NOT NULL
+    "emp_no" int   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "dept_no","emp_no"
+     )
 );
 
+-- Dept may have more than one manager.
+-- But one EE can have at most 1 mgr role.
+-- Composite Key, since both keys combine.
 CREATE TABLE "employees" (
     "emp_no" int   NOT NULL,
-    "emp_title" VARCHAR(5)   NOT NULL,
+    "emp_title_id" VARCHAR(5)   NOT NULL,
     "birth_date" date   NOT NULL,
     "first_name" VARCHAR(30)   NOT NULL,
     "last_name" VARCHAR(30)   NOT NULL,
@@ -36,11 +49,16 @@ CREATE TABLE "employees" (
      )
 );
 
+-- EEs may have same titles
 CREATE TABLE "salaries" (
     "emp_no" int   NOT NULL,
-    "salary" int   NOT NULL
+    "salary" int   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "emp_no"
+     )
 );
 
+-- One EE to one salary
 CREATE TABLE "titles" (
     "title_id" VARCHAR(5)   NOT NULL,
     "title" VARCHAR(30)   NOT NULL,
@@ -61,8 +79,9 @@ REFERENCES "departments" ("dept_no");
 ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
-ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title" FOREIGN KEY("emp_title")
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
 REFERENCES "titles" ("title_id");
 
 ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
+
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # sql-challenge
 SQL HW
+
+Inputs/Outputs in EmployeesSQL folder as main directory.
+
+This exercise deals with 6 csv files, representing data for an organization:
+*employees
+*dept_emp
+*departments
+*dept_manager
+*titles
+*salaries
+
+The relationships between these tables are displayed visually using ERD in 0_Data_Modeling.png.
+
+The model was exported and used to create the tables in Postgres using 1_Data_Engineering.sql
+
+Several queries of this data were recorded in 2_Employees_Data_Analysis.sql. Interesting concepts: JOINS
+
+The BONUS is answered in 3_Employees_Bonus.ipynb.  
+Interesting concepts: Creating a dictionary of tables as pandas dataframes, junction tables.
